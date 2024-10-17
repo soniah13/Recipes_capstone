5f6ac8e4b2eababb1b2da9ffc2438816edc47173
imorgansonia <imorgansonia@gmail.com>
2024-10-17 22:19:34 +0300

    most functinalities are working

--- Recipe_frontend/Recipe_react/src/Components/Form.jsx
+++ Recipe_frontend/Recipe_react/src/Components/Form.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import api from "../api";
 import { Link, useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 
@@ -17,10 +16,21 @@ function Form({ route, method }) {
         e.preventDefault();
 
         try {
-            const res = await api.post(route, { username, password });
+            const res = await fetch(route,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                }, 
+                body: JSON.stringify({username, password}),
+            });
+            if (!res.ok) {
+                throw new Error('Nerwork response was not okay');
+            }
+            const data = await res.json();
+
             if (method === "login") {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access);
-                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                localStorage.setItem(ACCESS_TOKEN, data.access);
+                localStorage.setItem(REFRESH_TOKEN, data.refresh);
                 navigate("/");
             } else {
                 navigate("/login");
@@ -39,19 +49,11 @@ function Form({ route, method }) {
         >
             <h1 className="text-2xl font-bold mb-6 text-center">{name}</h1>
             <input
-                className="w-full p-3 mb-4 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Username"
-            />
+                className="w-full p-3 mb-4 border border-gray-300 rounded focus:outline-none focus:border-blue-500" type="text"
+                value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username"/>
             <input
-                className="w-full p-3 mb-4 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-            />
+                className="w-full p-3 mb-4 border border-gray-300 rounded focus:outline-none focus:border-blue-500" type="password"
+                value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"/>
             <p className="p-8 font-semibold">Not Registered <span className="text-blue-500"><Link to='/register'>Register</Link></span></p>
             <button
                 className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition duration-300"
--- Recipe_frontend/Recipe_react/src/Components/SupriseMe.jsx
+++ Recipe_frontend/Recipe_react/src/Components/SupriseMe.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { ACCESS_TOKEN } from '../constants';
+
 
 function SupriseMe() {
     const [recipe, setRecipe] = useState(null);
@@ -8,18 +8,47 @@ function SupriseMe() {
 
     useEffect(() => {
         const fetchRandomRecipe = async () => {
+            const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    
+            if (!accessToken) {
+                console.error("No access token found");
+                setLoading(false);
+                return;
+            }
+    
             try {
-                const response = await axios.get('http://127.0.0.1:8000/api/v1/recipes/');
-                setRecipe(response.data.recipe);
+                const response = await fetch('http://127.0.0.1:8000/api/v1/recipes/', {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    }
+                });
+    
+                if (!response.ok) {
+                    throw new Error('Error fetching recipe');
+                }
+    
+                const data = await response.json();
+    
+                // Check if recipes array is returned
+                if (Array.isArray(data) && data.length > 0) {
+                    // Randomly select a recipe from the list
+                    const randomRecipe = data[Math.floor(Math.random() * data.length)];
+                    setRecipe(randomRecipe); // Set the random recipe
+                } else {
+                    console.log('No recipes available');
+                    setRecipe(null);
+                }
+    
             } catch (error) {
                 console.error('Error fetching recipe:', error);
             } finally {
                 setLoading(false);
             }
         };
+    
         fetchRandomRecipe();
     }, []);
-
+    
     return (
         <div className='m-0 p-0 border border-solid border-2  w-full h-screen'>
             <div className='flex center justify-center'>
@@ -29,23 +58,22 @@ function SupriseMe() {
             <p className='text-center font-bold text-2xl my-5 py-10'>Loading...</p>
         ) : recipe ? (
             <div>
-                <h1 className='text-center text-3xl font-bold underline'>{recipe.Name}</h1>
+                <h1 className='text-center text-3xl font-bold underline'>{recipe.name}</h1>
                 <div className='grid grid-cols-2 grid-rows-1'>
                     <div className='flex justify-center align-center'>
-                        <img src={recipe.image} alt={recipe.Name} className='w-5/6 h-5/6 center object-cover my-10' />
+                        <img src='https://res.cloudinary.com/ddqkfdqy8/image/upload/v1729161327/kdx5eina3m3vs4s7vkd0.png' alt={recipe.name} className='w-5/6 h-5/6 center object-cover my-10' />
                     </div>
                     <div className='p-10'>
-                        <p className='text-xl text-left p-2'><strong>Category:</strong> {recipe.category}</p>
-                        <p className='text-xl text-left p-2'><strong>Preparation Time:</strong> {recipe.prep}</p>
+                        <p className='text-xl text-left p-2'><strong>Preparation Time:</strong> {recipe.prep_time}</p>
                         <p className='text-xl text-left p-2'><strong>Serving:</strong> {recipe.serving}</p>
                         <h2 className='text-xl text-left font-bold mb-2 p-2'>Ingredients</h2>
                         <ul className='list-disc ml-5 mb-4 p-2'>
-                            {recipe.Ingredients && recipe.Ingredients.split(',').map((ingredient, index) => (
+                            {recipe.ingredients && recipe.ingredients.split(',').map((ingredient, index) => (
                                 <li key={index}>{ingredient}</li>
                             ))}
                         </ul>
                         <h3 className='text-xl text-left font-bold mb-2 p-2'>Procedure</h3>
-                        <p>{recipe.Procedure}</p>
+                        <p>{recipe.procedure}</p>
                     </div>
                 </div>
             </div>
